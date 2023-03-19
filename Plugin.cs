58f9d138311bdf586726168eaf75335e37d1846c
TastyChickenLegs <118636411+tastychickenleg@users.noreply.github.com>
2023-03-19 12:15:50 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+
+namespace SuperConfigurablePickupRadius
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class SuperConfigurablePickupRadiusPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "SuperConfigurablePickupRadius";
+        internal const string ModVersion = "1.0.4";
+        internal const string Author = "TastyChickenLegs";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        public static ConfigEntry<int> pickUpRange = null;
+        public static ConfigEntry<bool> enablePickUpRange = null;
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource SuperConfigurablePickupRadiusLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+            enablePickUpRange = config("Insanely Configurable", "Enable Pickup Range", true, "Enable Pickup Range");
+            pickUpRange = config("Insanely Configurable", "Pickup Range", 3, 
+                new ConfigDescription("Auto Pickup Distance from Player. Restart Game After Setting This", 
+                new AcceptableValueRange<int>(1, 10)));
+
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                SuperConfigurablePickupRadiusLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+                updateConfigs();
+            }
+            catch
+            {
+                SuperConfigurablePickupRadiusLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                SuperConfigurablePickupRadiusLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+        
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
+        }
+
+
+        #endregion
+        [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+        public static class Player_Awake_Patch
+        {
+            private static void Postfix(ref Player __instance)
+            {
+                if (enablePickUpRange.Value)
+                {
+                    //convert int to float and set the pickup range
+                    float autoPickupFloat = Convert.ToSingle(pickUpRange.Value);
+                    __instance.m_autoPickupRange = autoPickupFloat;
+                }
+            }
+        }
+
+        public static void updateConfigs()
+        {
+            float autoPickupFloat = Convert.ToSingle(pickUpRange.Value);
+            Player.m_localPlayer.m_autoPickupRange = autoPickupFloat;
+
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+### Super Configurable Pickup Radius
+by TastyChickenLegs
+
+
+Configure every aspect of your player pickup radius 
+
+```
+Configuration Options: - Insanely Configurable
+
+- Enable Pickup radius default is true
+- Pickup Radius - Configure Distance  Default is 3
+- ServerSync to push changes down to the players
+```
+
+Yes, I realize this is rediculous.... 
+
+1.0.4
+
+Added server sync to push config down to the players if on server
+This was due to server owners thinking the pickup value of 10 could be abused to take stuff from a distance.
+
+
+1.0.3
+
+- updated to newest Valheim patch
+
+1.0.2
+
+- Made incompatible with Valheim Plus because I have no respect for their developers or mod.  
+- Added ability to increase distance to 10
+
+
+1.0.1
+
+- made config file default to Enabled True and Radius 3
+
+1.0.0
+
+- Initial Release
+
+_____
+##	Now for the shameless plug
+
+> ### My Other Mods:
+>>* [No Smoke Stay Lit](https://valheim.thunderstore.io/package/TastyChickenLeg/NoSmokeStayLit/)
+>>* [No Smoke Simplified](https://valheim.thunderstore.io/package/TastyChickenLegs/NoSmokeSimplified/)
+>>* [Honey Please](https://valheim.thunderstore.io/package/TastyChickenLegs/HoneyPlease/)
+>>* [Automatic Fuel](https://valheim.thunderstore.io/package/TastyChickenLeg/AutomaticFuel/)
+>>* [Forsaken Powers Plus](https://valheim.thunderstore.io/package/TastyChickenLeg/ForsakenPowersPlus/)
+>>* [Recycle Plus](https://valheim.thunderstore.io/package/TastyChickenLeg/RecyclePlus/)
+>>* [Blast Furnace Takes All](https://valheim.thunderstore.io/package/TastyChickenLeg/BlastFurnaceTakesAll/)
+>>* [Timed Torches Stay Lit](https://valheim.thunderstore.io/package/TastyChickenLeg/TimedTorchesStayLit/)
+>>* [Drop More Loot](https://valheim.thunderstore.io/package/TastyChickenLegs/DropMoreLoot/)
